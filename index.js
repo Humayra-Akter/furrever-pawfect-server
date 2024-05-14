f6a2d9bc1fbcb4a00a559512bb427a9cb4536045
Humayra Akter <96544744+Humayra-Akter@users.noreply.github.com>
2024-05-14 23:26:53 +0600

    index

---
+++ index.js
@@ -0,0 +1,58 @@
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const app = express();
+require("dotenv").config();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.q18ojdg.mongodb.net/furrever-pawfect?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+
+
+async function run() {
+  try {
+    await client.connect();
+    
+    const userCollection = client.db("furrever-pawfect").collection("user");
+ 
+
+    // user post
+    app.post("/user", async (req, res) => {
+      const user = req.body;
+      const result = await userCollection.insertOne(user);
+      if (result.insertedCount === 1) {
+        res.status(201).json({ message: "user added successfully" });
+      } else {
+        res.status(500).json({ message: "Failed to add user" });
+      }
+    });
+
+    //user get
+    app.get("/user", async (req, res) => {
+      const query = {};
+      const cursor = userCollection.find(query);
+      const users = await cursor.toArray();
+      res.send(users);
+    });
+
+    
+  } finally {
+  }
+}
+
+run().catch(console.dir);
+
+app.listen(port, () => {
+  console.log(`server running on ${port}`);
+});
