621b113915caa5c573869d9bf01b7bf158863d54
Humayra Akter <96544744+Humayra-Akter@users.noreply.github.com>
2024-05-16 23:14:19 +0600

    bcrypt added

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const bcrypt = require("bcrypt");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 require("dotenv").config();
@@ -8,8 +9,8 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.guksi.mongodb.net/furrever-pawfect?retryWrites=true&w=majority&appName=Cluster0`;
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.q18ojdg.mongodb.net/furrever-pawfect?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -18,25 +19,25 @@ const client = new MongoClient(uri, {
   },
 });
 
-
-
 async function run() {
   try {
     await client.connect();
-    
+
     const userCollection = client.db("furrever-pawfect").collection("user");
- 
 
     // user post
-    app.post("/user", async (req, res) => {
-      const user = req.body;
-      const result = await userCollection.insertOne(user);
-      if (result.insertedCount === 1) {
-        res.status(201).json({ message: "user added successfully" });
-      } else {
-        res.status(500).json({ message: "Failed to add user" });
-      }
-    });
+     app.post("/user", async (req, res) => {
+       const { name, email, password, role } = req.body;
+       const hashedPassword = await bcrypt.hash(password, 10);
+       const user = { name, email, password: hashedPassword, role };
+       const result = await userCollection.insertOne(user);
+
+       if (result.insertedCount === 1) {
+         res.status(201).json({ message: "User added successfully" });
+       } else {
+         res.status(500).json({ message: "Failed to add user" });
+       }
+     });
 
     //user get
     app.get("/user", async (req, res) => {
@@ -45,8 +46,6 @@ async function run() {
       const users = await cursor.toArray();
       res.send(users);
     });
-
-    
   } finally {
   }
 }
